e2a265986c27a8b67ac99c032bcb1c5a75b9f983
2jun0 <soo28819@gmail.com>
2023-07-04 03:37:03 +0000

    IMR-7 feat : add router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import logo from './logo.png';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './Home';
+import MusicRecommend from './MusicRecommend';
+import './App.css';
 
 export default (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        <code>src/App.js</code>을 수정하고 저장하면 화면이 리로드 됩니다.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        리엑트 배우기 고고!
-      </a>
-    </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/music-rec" element={<MusicRecommend />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
