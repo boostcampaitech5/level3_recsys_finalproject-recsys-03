6e340cd9e7cbdc2a451ba4f47f6f3d48ee305c8e
kCMI113 <cmkang1316@gmail.com>
2023-07-05 23:02:10 +0900

    IMR-18 feat: modify loading page

--- front/src/components/page/Loading.jsx
+++ front/src/components/page/Loading.jsx
@@ -1,19 +1,18 @@
 import React, { PureComponent } from 'react';
+import './Loading.css';
 
 export default class Loading extends PureComponent {
   render() {
     return (
-      <header className="Loading-header">
-        <h1 style={{ color: '#FFF', paddingTop: '270px' }}>
-          ⭐️ 곧 결과가 나옵니다 ⭐️
-        </h1>
-        <h3 style={{ color: '#FFF' }}> 잠시만 기다려 주세요 ^~^</h3>
+      <div className="Loading-div">
+        <h1>⭐️ 곧 결과가 나옵니다 ⭐️</h1>
+        <h3> 잠시만 기다려 주세요 ^~^</h3>
         <img
-          width="80px"
-          src={`${process.env.PUBLIC_URL}/spinner.gif`}
+          className="Spinner"
+          src={`${process.env.PUBLIC_URL}/spining.svg`}
           alt="spinner"
         />
-      </header>
+      </div>
     );
   }
 }
