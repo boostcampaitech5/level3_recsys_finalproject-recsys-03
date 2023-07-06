74a2b670007eec7bfb34ac244d33d7735d44281f
2jun0 <soo28819@gmail.com>
2023-07-06 12:06:01 +0900

    IMR-22 feat : impliment genre selector

--- front/src/components/page/GenreSelectPage.jsx
+++ front/src/components/page/GenreSelectPage.jsx
@@ -1,5 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, createRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './GenreSelectPage.css';
+import SelectButton from './SelectButton';
 
 const genres = [
   {
@@ -28,17 +31,45 @@ const genres = [
   },
 ];
 
-export default class GenreSelector extends PureComponent {
+class GenreSelectorPage extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.nxtButton = createRef();
+  }
+
+  onSubmit() {
+    const { navigate } = this.props;
+    navigate('/loading');
+  }
+
   render() {
     return (
       <div className="GenreSelector">
         {genres.map((genre) => (
-          <div>
+          <SelectButton>
             <img src={process.env.PUBLIC_URL + genre.img} alt={genre.type} />
             <p>{genre.type}</p>
-          </div>
+          </SelectButton>
         ))}
+        <button
+          ref={this.nxtButton}
+          onClick={() => this.onSubmit()}
+          type="submit"
+        >
+          다음으로
+        </button>
       </div>
     );
   }
+}
+
+GenreSelectorPage.propTypes = {
+  navigate: PropTypes.func.isRequired,
+};
+
+export default function GenreSelectorPageWrapper(props) {
+  const navigate = useNavigate();
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <GenreSelectorPage {...props} navigate={navigate} />;
 }
