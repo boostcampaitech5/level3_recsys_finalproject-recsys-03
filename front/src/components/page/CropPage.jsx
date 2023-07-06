a647d9fdd7d408a86fb3e61ddc02da555c0dee04
DyeonPark <yeon0729@sookmyung.ac.kr>
2023-07-06 12:22:17 +0000

    IMG-19 refactor: change paging order

--- front/src/components/page/CropPage.jsx
+++ front/src/components/page/CropPage.jsx
@@ -1,21 +1,26 @@
 import React, { useCallback, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Cropper from 'react-easy-crop';
 import './CropPage.css';
-// import generateDownload from '../utils/generateDownload';
 
 function CropPage() {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
 
   const location = useLocation();
-  const Img = { ...location.state };
-  const imgsrc = URL.createObjectURL(Img.url);
+  const info = { ...location.state };
+  const imgurl = URL.createObjectURL(info.url);
 
   // eslint-disable-next-line no-unused-vars
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     // console.log(croppedArea, croppedAreaPixels);
   }, []);
+
+  const navigate = useNavigate();
+  const naviagteToNext = () => {
+    // sending data(cropped img, genres) must be added here
+    navigate('/loading');
+  };
 
   return (
     <div>
@@ -23,7 +28,7 @@ function CropPage() {
       <div className="container-cropper">
         <div className="cropper">
           <Cropper
-            image={imgsrc}
+            image={imgurl}
             crop={crop}
             zoom={zoom}
             aspect={3 / 3}
@@ -35,7 +40,9 @@ function CropPage() {
       </div>
       <div className="buttons">
         <button type="button">다시 선택</button>
-        <button type="button">결과보기</button>
+        <button type="button" onClick={naviagteToNext}>
+          결과보기
+        </button>
       </div>
     </div>
   );
