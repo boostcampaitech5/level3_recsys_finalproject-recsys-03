0b5206ad566b079ebdff058874de59eeb3fd557a
DyeonPark <yeon0729@sookmyung.ac.kr>
2023-07-06 11:30:35 +0000

    IMR-19 fix: set css of cropping to size error

--- front/src/components/page/CropPage.jsx
+++ front/src/components/page/CropPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Cropper from 'react-easy-crop';
 import './CropPage.css';
@@ -12,26 +12,28 @@ function CropPage() {
   const Img = { ...location.state };
   const imgsrc = URL.createObjectURL(Img.url);
 
-  const onCropComplete = (croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels);
-  };
+  // eslint-disable-next-line no-unused-vars
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    // console.log(croppedArea, croppedAreaPixels);
+  }, []);
 
   return (
     <div>
+      <div className="page-title">이미지를 잘라주세요.</div>
       <div className="container-cropper">
         <div className="cropper">
           <Cropper
             image={imgsrc}
             crop={crop}
             zoom={zoom}
-            aspect={1}
+            aspect={3 / 3}
             onCropChange={setCrop}
             onCropComplete={onCropComplete}
             onZoomChange={setZoom}
           />
         </div>
       </div>
-      <div>
+      <div className="buttons">
         <button type="button">다시 선택</button>
         <button type="button">결과보기</button>
       </div>
