5776bc8974cd0bdf50bf58911a144c50e3d23977
2jun0 <soo28819@gmail.com>
2023-07-07 06:48:14 +0000

    IMR-29 feat : add header in music recommend page and apply integrated style

--- front/src/components/page/MusicRecommend.jsx
+++ front/src/components/page/MusicRecommend.jsx
@@ -1,43 +1,41 @@
-import React, { PureComponent, createRef } from 'react';
+import React, { PureComponent } from 'react';
 import MusicSelector from './MusicSelector';
-import MusicDetail from './MusicDetail';
 import './MusicRecommend.css';
-import queryImg from './dummy-4.jpg';
 
 const songInfos = [
   {
     videoYtId: 'XHMdIA6bEOE',
-    src: '/dummy-5.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '짱구는 못말려 오프닝',
     artistName: '아이브',
   },
   {
     videoYtId: '_sQhN4dLC60',
-    src: '/dummy-4.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '첫사랑',
     artistName: '버스커 버스커',
   },
   {
     videoYtId: 'y5MAgMVwfFs',
-    src: '/dummy-6.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '좋다고 말해',
     artistName: '볼빨간사춘기',
   },
   {
     videoYtId: 'XHMdIA6bEOE',
-    src: '/dummy-4.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '짱구는 못말려 오프닝',
     artistName: '아이브',
   },
   {
     videoYtId: '_sQhN4dLC60',
-    src: '/dummy-5.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '첫사랑',
     artistName: '버스커 버스커',
   },
   {
     videoYtId: 'y5MAgMVwfFs',
-    src: '/dummy-6.jpg',
+    src: '/dummy-1.jpg',
     musicTitle: '좋다고 말해',
     artistName: '볼빨간사춘기',
   },
@@ -47,23 +45,24 @@ export default class MusicRecommend extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.musicDetailRef = createRef();
-
     this.onSlideChange = (e) => {
+      // eslint-disable-next-line no-unused-vars
       const songInfo = songInfos[e.target.swiper.realIndex];
-      this.musicDetailRef.current.updateSongInfo(songInfo);
     };
   }
 
   render() {
     return (
       <div className="MusicRecommend">
-        <img width="500px" src={queryImg} alt="쿼리 이미지" />
+        <div className="header">
+          <h1>일상의 노래를 찾았어요!</h1>
+          <h3>당신의 일상과 취향을 모두 반영했어요</h3>
+        </div>
+
         <MusicSelector
           songInfos={songInfos}
           onSlideChange={this.onSlideChange}
         />
-        <MusicDetail ref={this.musicDetailRef} songInfo={songInfos[0]} />
         <button type="button">결과 공유하기</button>
       </div>
     );
