4f0befece4b61d869e1a3a12710f0f8d690132f0
2jun0 <soo28819@gmail.com>
2023-07-07 17:17:18 +0000

    IMR-33 feat : apply css to crop page and add cropimage feat

--- front/src/components/page/CropPage.jsx
+++ front/src/components/page/CropPage.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Cropper from 'react-easy-crop';
 import './CropPage.css';
+import getCroppedImg from '../../utils/cropImage';
 
 function CropPage() {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
@@ -9,27 +10,40 @@ function CropPage() {
 
   const location = useLocation();
   const info = { ...location.state };
-  const imgurl = URL.createObjectURL(info.url);
+  const [imgurl] = useState(() => URL.createObjectURL(info.url));
+  const [croppedImage, setCroppedImage] = useState();
 
-  // eslint-disable-next-line no-unused-vars
-  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    // console.log(croppedArea, croppedAreaPixels);
-  }, []);
+  const onCropComplete = useCallback(
+    async (croppedArea, croppedAreaPixels) => {
+      const croppedImageTmp = await getCroppedImg(imgurl, croppedAreaPixels);
+      setCroppedImage(croppedImageTmp);
+    },
+    [imgurl, setCroppedImage]
+  );
 
   const navigate = useNavigate();
   const naviagteToNext = () => {
     // sending data(cropped img, genres) must be added here
     navigate('/loading');
   };
+  const goUploadPage = () => {
+    // sending data(cropped img, genres) must be added here
+    navigate('/upload', {
+      state: {
+        url: croppedImage,
+        genres: info.genres,
+      },
+    });
+  };
 
   return (
-    <div>
-      <div className="header">
-        <h1 className="title">이미지를 편집해주세요</h1>
-        <h3 className="subtitle">박스를 움직여 원하는 영역을 지정해주세요</h3>
-      </div>
-
+    <div className="CropPage">
       <div className="contents">
+        <div className="header">
+          <h1>이미지를 편집해주세요</h1>
+          <h3>박스를 움직여 원하는 영역을 지정해주세요</h3>
+        </div>
+
         <div className="container-cropper">
           <div className="cropper">
             <Cropper
@@ -37,6 +51,7 @@ function CropPage() {
               crop={crop}
               zoom={zoom}
               aspect={3 / 3}
+              maxZoom={3}
               onCropChange={setCrop}
               onCropComplete={onCropComplete}
               onZoomChange={setZoom}
@@ -46,12 +61,38 @@ function CropPage() {
       </div>
 
       <div className="footer">
-        <button type="button" onClick={() => navigate(-1)}>
-          다시 선택
-        </button>
-        <button type="button" onClick={naviagteToNext}>
-          결과보기
-        </button>
+        <div className="buttons">
+          <button
+            className="retry"
+            type="button"
+            onClick={() => goUploadPage()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+            >
+              <path d="M480-160q-133 0-226.5-93.5T160-480q0-133 93.5-226.5T480-800q85 0 149 34.5T740-671v-129h60v254H546v-60h168q-38-60-97-97t-137-37q-109 0-184.5 75.5T220-480q0 109 75.5 184.5T480-220q83 0 152-47.5T728-393h62q-29 105-115 169t-195 64Z" />
+            </svg>
+            다시선택
+          </button>
+          <button
+            className="next"
+            type="button"
+            onClick={() => naviagteToNext()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+            >
+              <path d="M220-40q-24 0-42-18t-18-42v-509q0-24 18-42t42-18h169v60H220v509h520v-509H569v-60h171q24 0 42 18t18 42v509q0 24-18 42t-42 18H220Zm229-307v-457l-88 88-43-43 161-161 161 161-43 43-88-88v457h-60Z" />
+            </svg>
+            결과 보기
+          </button>
+        </div>
       </div>
     </div>
   );
