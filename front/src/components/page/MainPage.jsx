57763a84630327b17b5d3dd02368f66b61fe7022
DyeonPark <yeon0729@sookmyung.ac.kr>
2023-07-07 03:23:50 +0000

    IMR-26 feat: apply css in MainPage

--- front/src/components/page/MainPage.jsx
+++ front/src/components/page/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './MainPage.css';
 
 function MainPage() {
   const navigate = useNavigate();
@@ -9,10 +10,24 @@ function MainPage() {
 
   return (
     <div className="bg">
-      <div className="Title">서비스명</div>
+      <div className="space" />
+      <div className="line" />
+      <div className="title">
+        당신의 <span className="colored">일상</span>에 <br />
+        <span className="colored">노래</span>를 <br /> 찾아드립니다
+      </div>
+      <div className="line" />
+      <div className="subtitle">
+        AI가 당신의 일상 사진에
+        <br />
+        어울리는 노래를 찾아드립니다 <br />
+        <br />
+        <span className="border">내 일상의 BGM을 만나보세요</span>
+      </div>
+      <div className="space" />
       <div className="NextPageBnt">
-        <button type="button" onClick={naviateToUpload}>
-          시작하기
+        <button className="starbBnt" type="button" onClick={naviateToUpload}>
+          <span className="bntText1">시작하기</span>
         </button>
       </div>
     </div>
