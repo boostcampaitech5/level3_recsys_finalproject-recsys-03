36d0f8367fe3ce06a2ea25c9596d69f9c4b68af8
kCMI113 <cmkang1316@gmail.com>
2023-07-09 06:35:12 +0000

    IMR-35 feat: add generated img to modal

--- front/src/components/modal/Modal.jsx
+++ front/src/components/modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Modal.css';
 import PropTypes from 'prop-types';
-// import { saveAs } from 'file-saver';
+import CardGen from './CardGenerator';
 
 function Modal({ setOpenModal }) {
   return (
@@ -25,26 +25,7 @@ function Modal({ setOpenModal }) {
             </svg>
           </button>
         </div>
-        <div className="title">
-          <img
-            className="cardImg"
-            src={`${process.env.PUBLIC_URL}/dummy-music-card.png`}
-            alt="music-card"
-          />
-        </div>
-        <div className="bottom">
-          <button className="downloadBnt" type="button">
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="35"
-              viewBox="0 -960 960 960"
-              width="35"
-            >
-              <path d="M220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Zm260-153L287-506l43-43 120 120v-371h60v371l120-120 43 43-193 193Z" />
-            </svg> */}
-            <span>이미지 다운로드</span>
-          </button>
-        </div>
+        <CardGen />
       </div>
     </div>
   );
