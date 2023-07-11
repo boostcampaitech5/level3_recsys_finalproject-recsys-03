e98df41800c374e1b2704cfeb3d6061300e5ea16
alstjrdlzz <tjalstjr95@gmail.com>
2023-07-11 04:27:35 +0000

    IMR-52 fix: indexing input, output path format

--- ml/run_indexing.py
+++ ml/run_indexing.py
@@ -1,8 +1,9 @@
+import os
 import hydra
+import datasets
 from transformers import AutoImageProcessor, AutoModel
 from src.utils import (
     set_seed,
-    load_huggingface_dataset,
     encode,
 )
 
@@ -10,22 +11,29 @@ from src.utils import (
 def main(config) -> None:
     # set
     name = config.name
+    data_dir = config.data_dir
+    output_dir = config.output_dir
+    tag_type = config.tag_type
+    save_dir = os.path.join(output_dir, name)
     set_seed(config.seed)
+
     # init model
-    ckpt = f"./output/{name}_huggingface"
+    ckpt_dirpath = os.path.join(output_dir, name)
+    ckpt_filename = f"{name}_huggingface"
+    ckpt = os.path.join(ckpt_dirpath, ckpt_filename)
     processor = AutoImageProcessor.from_pretrained(ckpt)
     model = AutoModel.from_pretrained(ckpt)
     # load dataset
-    dataset = load_huggingface_dataset()
+    dataset = datasets.load_from_disk(os.path.join(data_dir, f"{tag_type}_dataset"))
     # encode
     dataset_with_embeddings = dataset.map(lambda x: {"embeddings": encode(x["image"], processor, model)})
     # add faiss index
     dataset_with_embeddings.add_faiss_index(column="embeddings")
     # save faiss index
-    dataset_with_embeddings.save_faiss_index(index_name="embeddings", file=f"./output/{name}/my_index.faiss")
+    dataset_with_embeddings.save_faiss_index(index_name="embeddings", file=os.path.join(save_dir, f"{tag_type}.index"))
     # save dataset
     dataset_with_embeddings.drop_index(index_name="embeddings")
-    dataset_with_embeddings.save_to_disk(f"./output/{name}/my_datasets")
+    dataset_with_embeddings.save_to_disk(os.path.join(save_dir, f"{tag_type}_dataset"))
 
 
 @hydra.main(version_base="1.2", config_path="configs/indexing", config_name="config.yaml")
