13580169e1a5fce202607ac5069cbc30902d39f0
kCMI113 <cmkang1316@gmail.com>
2023-07-19 03:39:35 +0000

    IMR-85 feat: change logger name to user_logger

--- back/src/router/SaveFile.py
+++ back/src/router/SaveFile.py
@@ -2,17 +2,16 @@ from fastapi import UploadFile
 from src.log.CreateDirectory import createDirectory
 from logging import Logger
 
-
 IMG_PATH = "outputs/userImgs/"
 
-def save_file(session_id:str, image: UploadFile, logger:Logger) -> None:
+def save_file(session_id:str, image: UploadFile, user_logger:Logger) -> None:
     createDirectory(IMG_PATH)
 
     file_path = f"{IMG_PATH}{session_id}.jpg"
     with open(file_path, "wb+") as file_object:
         file_object.write(image.file.read())
 
-    logger.info("Img Path : %s", file_path)
+    user_logger.info("Img Path : %s", file_path)
 
 
     
--- back/src/router/root_router.py
+++ back/src/router/root_router.py
@@ -4,11 +4,11 @@ from uuid import uuid4
 from src.router.dto.RecommendMusicRequest import RecommendMusicRequest
 from src.router.dto.RecommendMusicResponse import RecommendMusicResponse, RecommendMusic
 from src.infer.playlist import PlaylistIdExtractor
-from src.log.SetLogger import setLogger
+from src.log.Logger import get_user_logger
 from . import SaveFile
 router = APIRouter()
 
-logger = setLogger()
+user_logger = get_user_logger()
 
 pl_k = 3
 song_k = 6  # song_k must be more than 6 or loop of silder must be False
@@ -19,17 +19,17 @@ playlist = PlaylistIdExtractor(k=pl_k)
 async def recommend_music(
     image: UploadFile = File(...), data: RecommendMusicRequest = Depends(RecommendMusicRequest.as_form)
 ) -> RecommendMusicResponse:
-    
+
     session_id = str(uuid4()).replace("-","_")
-    logger.info("Session ID : %s", session_id)
-    SaveFile.save_file(session_id, image, logger)
-    logger.info("Genres : %s", data)
+    user_logger.info("Session ID : %s", session_id)
+    SaveFile.save_file(session_id, image, user_logger)
+    user_logger.info("Genres : %s", data)
 
     pl_ids = []
     pl_ids.extend(playlist.get_weather_playlist_id(image))
     # pl_ids.extend(playlist.get_mood_playlist_id(image))  # place for mood playlist id
     # pl_ids.extend(playlist.get_sit_playlist_id(image))  # place for situation playlist id
-    logger.info("Playlist IDs : %s", pl_ids)
+    user_logger.info("Playlist IDs : %s", pl_ids)
 
     # song_ids = get_songs_from_pls(pl_ids)
     # top_songs = get_top_songs_with_step_3(song_ids, side_info_like_genres)
@@ -55,7 +55,7 @@ async def recommend_music(
         RecommendMusic(song_id=5, youtube_id="2Kff0U8w-aU", song_title="OMG", artist_name="NewJeans", album_title="NewJeans 'OMG'"),
         RecommendMusic(song_id=6, youtube_id="j1uXcHwLhHM", song_title="사건의 지평선", artist_name="윤하", album_title="END THEORY : Final Edition"),
     ]
-    logger.info("Recommend Songs : %s",  songs)
+    user_logger.info("Recommend Songs : %s",  songs)
 
     return RecommendMusicResponse(session_id=session_id,songs=songs)
 
