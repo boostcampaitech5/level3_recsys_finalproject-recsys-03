5c6576eb549e96f842d8e638e934b4552f622a22
kCMI113 <cmkang1316@gmail.com>
2023-07-19 06:31:20 +0000

    IMR-100 feat: change format of user_log to json

--- back/src/log/Logger.py
+++ back/src/log/Logger.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+from pythonjsonlogger import jsonlogger
 from . import CreateDirectory
 
 OUTPUT_PATH = "outputs/logs"
@@ -7,47 +8,48 @@ USER_LOG_PATH = OUTPUT_PATH + "/user_log.log"
 API_LOG_PATH = OUTPUT_PATH + "/api_log.log"
 
 logger_config = {
-    "version" : 1,
+    "version": 1,
     "disable_existing_loggers": True,
-    "formatters" :{
-        "basic" : {"format" : "%(asctime)s | %(levelname)s - %(message)s"}
-    },
-    "handlers" : {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter" : "basic"
+    "formatters": {
+        "basic": {"format": "%(asctime)s | %(levelname)s - %(message)s"},
+        "json": {
+            "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
+            "format": "%(asctime)s %(levelname)s %(filename)s %(session_id)s %(Img Path)s %(Genres)s %(Playlist IDs)s %(Recommend Songs)s",
         },
+    },
+    "handlers": {
+        "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "basic"},
         "user_file": {
             "level": "INFO",
             "class": "logging.handlers.RotatingFileHandler",
             "mode": "a",
-            "formatter" : "basic",
+            "formatter": "json",
+            "encoding": "utf-8",
             "filename": USER_LOG_PATH,
-            "encoding": "UTF-8",
             "maxBytes": 1e7,
-            "backupCount": 100
+            "backupCount": 100,
         },
         "api_file": {
             "level": "INFO",
             "class": "logging.handlers.RotatingFileHandler",
             "mode": "a",
-            "formatter" : "basic",
+            "formatter": "basic",
+            "encoding": "utf-8",
             "filename": API_LOG_PATH,
-            "encoding": "UTF-8",
             "maxBytes": 1e7,
-            "backupCount": 100
-        }
+            "backupCount": 100,
+        },
     },
-    "loggers": {"fastapi":{"level": "INFO", "handlers":["api_file", "console"]},
-                "user":{"level": "INFO", "handlers":["user_file", "console"]}}
+    "loggers": {"fastapi": {"level": "INFO", "handlers": ["api_file", "console"]}, "user": {"level": "INFO", "handlers": ["user_file", "console"]}},
 }
 
 CreateDirectory.createDirectory(OUTPUT_PATH)
 logging.config.dictConfig(logger_config)
 
+
 def get_fastapi_logger() -> logging.Logger:
     return logging.getLogger("fastapi")
+
 
 def get_user_logger() -> logging.Logger:
     return logging.getLogger("user")
--- back/src/router/SaveFile.py
+++ back/src/router/SaveFile.py
@@ -4,14 +4,12 @@ from logging import Logger
 
 IMG_PATH = "outputs/userImgs/"
 
-def save_file(session_id:str, image: UploadFile, user_logger:Logger) -> None:
+
+def save_file(session_id: str, image: UploadFile, user_logger: Logger) -> str:
     createDirectory(IMG_PATH)
 
     file_path = f"{IMG_PATH}{session_id}.jpg"
     with open(file_path, "wb+") as file_object:
         file_object.write(image.file.read())
 
-    user_logger.info("Img Path : %s", file_path)
-
-
-    
+    return file_path
