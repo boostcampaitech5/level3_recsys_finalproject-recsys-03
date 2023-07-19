6280ed92009244efe03349e840b5281c32724cac
kCMI113 <cmkang1316@gmail.com>
2023-07-19 08:24:35 +0000

    IMR-97 refactor: delete duplicate modules

--- back/src/log/CreateDirectory.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def createDirectory(dir: str) -> None:
-    try:
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-            print(f">>> {dir} is created !!!")
-    except OSError:
-        Exception("Creating %s is failed !!!", dir)
--- back/src/utils.py
+++ back/src/utils.py
@@ -1,12 +1,13 @@
 import os
 
+
 def create_dir(path: str) -> None:
     try:
         if not os.path.exists(path):
             os.makedirs(path)
     except OSError:
-        print(f"Error: Creating Directory - {path}")
-        
+        Exception("Creating %s is failed !!!", dir)
+
 
 def get_first_dir(path: str) -> str:
     dirs = os.listdir(path)
