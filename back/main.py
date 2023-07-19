563441d2a025b7e75916a604019e1de0278aa493
kCMI113 <cmkang1316@gmail.com>
2023-07-19 03:41:15 +0000

    IMR-85 feat: add exception handler in main

--- back/main.py
+++ back/main.py
@@ -2,13 +2,14 @@ import uvicorn
 import os
 from fastapi import FastAPI
 from src.router import root_router
+from src.log import ExceptionHandler
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
 app = FastAPI()
 app.include_router(root_router.router)
-
+ExceptionHandler.add_exception_handler(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.environ["HOST"], port=int(os.environ["PORT"]))
