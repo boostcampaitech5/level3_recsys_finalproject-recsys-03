d902ed272e5e0477c80f2fb1156b9afd01a31ca7
kCMI113 <cmkang1316@gmail.com>
2023-07-23 15:14:18 +0000

    IMR-123 feat: add spotify query result validation logic

--- back/src/infer/spotify.py
+++ back/src/infer/spotify.py
@@ -2,6 +2,7 @@ import os
 import spotipy
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
+from src.utils import check_substring
 
 
 def get_spotify_url(df: pd.DataFrame, top_k: int) -> pd.DataFrame:
@@ -20,20 +21,36 @@ def get_spotify_url(df: pd.DataFrame, top_k: int) -> pd.DataFrame:
         artist = df.iloc[i]["artist_name"]
 
         seary_query = title + " " + artist
-        result = sp.search(seary_query, limit=1, type="track")
-        url = result["tracks"]["items"][0]["preview_url"]
+        result = sp.search(seary_query, limit=4, type="track")
 
-        if url:
-            urls.append(
-                {
-                    "song_id": df.iloc[i]["song_id"],
-                    "youtube_key": df.iloc[i]["youtube_key"],
-                    "song_title": df.iloc[i]["song_title"],
-                    "artist_name": df.iloc[i]["artist_name"],
-                    "album_title": df.iloc[i]["album_title"],
-                    "music_url": url,
-                }
-            )
+        for idx in range(4):
+            res_release_date = result["tracks"]["items"][idx]["album"]["release_date"]
+            res_artist_name = result["tracks"]["items"][idx]["artists"][0]["name"]
+            res_title = result["tracks"]["items"][idx]["name"]
+            url = result["tracks"]["items"][idx]["preview_url"]
+
+            if res_title != title and res_artist_name != artist:
+                print(f"spotify : {res_title},  {res_artist_name}")
+                print(f"genie : {title},  {artist}")
+                if not (check_substring(res_title, title) and check_substring(res_artist_name, artist)):
+                    print("wrong!!")
+                    print(f"spotify : {res_title},  {res_artist_name}")
+                    print(f"genie : {title},  {artist}")
+                    print("-----------------------------------------------")
+                    url = None
+
+            if url:
+                urls.append(
+                    {
+                        "song_id": df.iloc[i]["song_id"],
+                        "youtube_key": df.iloc[i]["youtube_key"],
+                        "song_title": df.iloc[i]["song_title"],
+                        "artist_name": df.iloc[i]["artist_name"],
+                        "album_title": df.iloc[i]["album_title"],
+                        "music_url": url,
+                    }
+                )
+                break
 
     df = pd.DataFrame(urls)
     return df
--- back/src/utils.py
+++ back/src/utils.py
@@ -19,3 +19,9 @@ def get_first_dir(path: str) -> str:
 def str2list(data: pd.DataFrame, columns: list) -> None:
     for col in columns:
         data[col] = data[col].apply(lambda x: ast.literal_eval(x))
+
+
+def check_substring(result: str, query: str) -> bool:
+    result = result.lower()
+    query = query.lower()
+    return result in query or query in result
