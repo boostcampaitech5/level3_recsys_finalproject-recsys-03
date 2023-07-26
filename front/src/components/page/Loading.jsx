56c396a037358fbe3a8df9fcd6237087ad37dba6
DyeonPark <yeon0729@sookmyung.ac.kr>
2023-07-26 09:03:34 +0000

    IMR-139 chore: change font size

--- front/src/components/page/Loading.jsx
+++ front/src/components/page/Loading.jsx
@@ -5,11 +5,11 @@ function Loading() {
   return (
     <div className="contents">
       <div className="Loading-div">
-        <h1 className="top-text">노래를 찾고있어요.</h1>
+        <h1 className="top-text">노래를 찾고있어요</h1>
         <p className="bott-text">
           10초 뒤면 결과를 볼 수 있습니다!
           <br />
-          화면을 이탈하면 오류가 날 수 있으니 조금만 기다려주세요.
+          화면을 이탈하면 오류가 날 수 있으니 조금만 기다려주세요
         </p>
         <br />
         <img
