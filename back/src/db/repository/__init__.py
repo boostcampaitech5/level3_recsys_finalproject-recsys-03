7f9a938af05aa9a093cd5317b5d4e9b7a2b02038
2jun0 <soo28819@gmail.com>
2023-08-09 18:13:32 +0000

    IMR-159 feat : add auth repository

--- back/src/db/repository/__init__.py
+++ back/src/db/repository/__init__.py
@@ -3,5 +3,6 @@ from .inference import InferenceRepository
 from .playlist import PlaylistRepository
 from .song import SongRepository
 from .user import UserRepository
+from .auth import AuthRepository
 
-__all__ = [ArtistRepository, InferenceRepository, PlaylistRepository, SongRepository, UserRepository]
+__all__ = [ArtistRepository, InferenceRepository, PlaylistRepository, SongRepository, UserRepository, AuthRepository]
---
+++ back/src/db/repository/auth.py
@@ -0,0 +1,27 @@
+from mongoengine import QuerySet
+from ..document import AuthDocument
+from ..exception import NotFoundAuthException
+from ...dto.model import Auth, User
+
+
+class AuthRepository:
+    def create_auth(self, user: User, refresh_token: str) -> Auth:
+        auth = AuthDocument(user=user, refresh_token=refresh_token)
+        saved: AuthDocument = auth.save()
+        return saved.to_dto()
+
+    def find_by_user_id(self, user_id: str) -> Auth:
+        auth: AuthDocument = AuthDocument.objects(user__id=user_id).first()
+
+        if not auth:
+            return None
+
+        return auth.to_dto()
+
+    def delete_by_id(self, id: str) -> None:
+        auths: QuerySet[AuthDocument] = AuthDocument.objects(id=id)
+
+        if not auths:
+            raise NotFoundAuthException(f"Can't find auth document: id={id}")
+
+        auths.delete()
