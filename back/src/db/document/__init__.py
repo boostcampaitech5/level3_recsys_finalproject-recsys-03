e105f5a8962a517009ee3c96c4f6dbc86edc6e14
2jun0 <soo28819@gmail.com>
2023-08-09 17:36:40 +0000

    IMR-159 feat : add auth doc in back

--- back/src/db/document/__init__.py
+++ back/src/db/document/__init__.py
@@ -3,5 +3,6 @@ from .inference import InferenceDocument, SongsElementEmbeddedDocument
 from .playlist import PlaylistDocument
 from .song import SongDocument
 from .user import UserDocument
+from .auth import AuthDocument
 
-__all__ = [ArtistDocument, InferenceDocument, SongsElementEmbeddedDocument, PlaylistDocument, SongDocument, UserDocument]
+__all__ = [ArtistDocument, InferenceDocument, SongsElementEmbeddedDocument, PlaylistDocument, SongDocument, UserDocument, AuthDocument]
---
+++ back/src/db/document/auth.py
@@ -0,0 +1,18 @@
+from mongoengine import Document, DateTimeField, StringField, ReferenceField
+from ...dto.model import Auth
+from datetime import datetime
+from ...config import AppConfig
+from .user import UserDocument
+
+config = AppConfig()
+
+
+class AuthDocument(Document):
+    user = ReferenceField(UserDocument, required=True)
+    refresh_token = StringField(required=True)
+    created_at = DateTimeField(default=datetime.utcnow)
+
+    meta = {"indexes": [{"fields": ["created_at"], "expireAfterSeconds": config.refresh_token_exp_period}]}
+
+    def to_dto(self) -> Auth:
+        return Auth(id=str(self.id), user=self.user.to_dto(), refresh_token=self.refresh_token, created_at=self.created_at)
