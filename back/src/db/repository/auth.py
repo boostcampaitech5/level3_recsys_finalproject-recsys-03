818bfc95dfc3028d97852b6950f1e327fd7dc17a
2jun0 <soo28819@gmail.com>
2023-08-10 08:02:33 +0000

    IMR-159 fix : convert user dto to user doc in AuthRepository.create_auth

--- back/src/db/repository/auth.py
+++ back/src/db/repository/auth.py
@@ -1,12 +1,14 @@
 from mongoengine import QuerySet
-from ..document import AuthDocument
+from ..document import AuthDocument, UserDocument
 from ..exception import NotFoundAuthException
 from ...dto.model import Auth, User
+from .common import find_user_doc_by_dto
 
 
 class AuthRepository:
     def create_auth(self, user: User, refresh_token: str) -> Auth:
-        auth = AuthDocument(user=user, refresh_token=refresh_token)
+        user_doc: UserDocument = find_user_doc_by_dto(user)
+        auth = AuthDocument(user=user_doc, refresh_token=refresh_token)
         saved: AuthDocument = auth.save()
         return saved.to_dto()
 
