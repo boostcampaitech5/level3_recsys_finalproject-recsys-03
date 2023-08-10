300965058ca6ebb604b67572bb1ba7d906f259d4
2jun0 <soo28819@gmail.com>
2023-08-10 13:12:02 +0000

    IMR-159 fix : add CreatedAtMixin to AuthDocument

--- back/src/db/document/auth.py
+++ back/src/db/document/auth.py
@@ -1,16 +1,17 @@
-from mongoengine import Document, DateTimeField, StringField, ReferenceField
+from mongoengine import Document, StringField, ReferenceField
 from ...dto.model import Auth
-from datetime import datetime
+
+# from datetime import datetime
 from ...config import AppConfig
 from .user import UserDocument
+from .mixin.date import CreatedAtMixin
 
 config = AppConfig()
 
 
-class AuthDocument(Document):
+class AuthDocument(CreatedAtMixin, Document):
     user = ReferenceField(UserDocument, required=True, unique=True)
     refresh_token = StringField(required=True)
-    created_at = DateTimeField(default=datetime.utcnow)
 
     meta = {"indexes": [{"fields": ["created_at"], "expireAfterSeconds": int(config.refresh_token_exp_period.total_seconds())}]}
 
---
+++ back/src/db/document/mixin/date.py
@@ -0,0 +1,17 @@
+from mongoengine import DateTimeField
+from datetime import datetime
+
+
+def utcnow():
+    now = datetime.utcnow()
+    return now.replace(microsecond=round(now.microsecond, -3))
+
+
+class CreatedAtMixin:
+    created_at = DateTimeField(default=utcnow)
+    meta = {"abstract": True}
+
+
+class UpdatedAtMixin:
+    created_at = DateTimeField(default=utcnow)
+    meta = {"abstract": True}
