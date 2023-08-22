588cdc87b95701988ebeb2fa3da26d92e2508218
2jun0 <soo28819@gmail.com>
2023-08-23 01:42:34 +0900

    chore: remove back code

--- back/src/dto/feedback.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserFeedbackRequest(BaseModel):
-    session_id: str
-    song_id: int
-    is_like: bool
--- back/src/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def create_dir(path: str) -> None:
-    try:
-        if not os.path.exists(path):
-            os.makedirs(path)
-    except OSError:
-        raise Exception(f"Creating {path} is failed !!!")
